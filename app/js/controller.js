769a32184ba6eee9ba11c8a46371070b1f117ba3
Swar <swar12894@gmail.com>
2015-01-15 15:25:25 -0500

    Initial Commit

---
+++ app/js/controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatApp = angular.module('contactListApp', []);
+
+phonecatApp.controller('ContactListCtrl', function($scope) {
+  $scope.contacts = [
+    {'first': 'Jim',
+     'last': 'Cooper',
+	 'extension': '3219'},
+    {'first': 'Liane',
+     'last': 'Cooper',
+	 'extension': '3336'},
+    {'first': 'Rick',
+     'last': 'Brown',
+	 'extension': '3376'},
+	 {'first': 'Len',
+     'last': 'Payne',
+	 'extension': '3418'}
+  ];
+  $scope.orderProp = 'first';
+});
